23a8afcd32c0b550ec7941208ea3854e92613251
fhonsiah <fhonsiah@gmail.com>
2022-10-24 04:14:43 -0700

    I'm federal agent Jack Bauer, and today is the longest day of my life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,45 @@
+/*
+ *Program:8_24_hours.c
+ *Afunction that prints every minute of the day of Jack Bauer,starting from
+ *00:00 to 23:59.
+ *Author:Faustina Honsiah <fhonsiah@gmail.com>
+ *Copyright @2022
+ */
+#include "main.h"
+/**
+ *Jack_bauer - prints the last digit of a number
+ *
+ *@void: Does return anything.
+ *
+ *Retrun:the retrun type is void i.e does not return anything.
+ *
+ */
+void jack_bauer(void)
+{
+	int hours;
+	int hour_digit1;
+	int hour_digit2;
+	int minutes;
+	int minute_digit1;
+	int minute_digit2;
+
+	for (hours = 0; hours < 24; hours++)
+	{
+		hour_digit1 = hours / 10;
+		hour_digit2 = hours % 10;
+		for (minutes = 0; minutes < 60; minutes++)
+		{
+			minute_digit1 = minutes / 10;
+			minute_digit2 = minutes % 10;
+
+			_putchar(hour_digit1 + '0');
+			_putchar(hour_digit2 + '0');
+			_putchar(':');
+			_putchar(minute_digit1 + '0');
+			_putchar(minute_digit2 + '0');
+			_putchar('\n');
+		}
+		minutes = 0; /*Reset minutes to get minutes starting from 0*/
+		
+	}
+}
