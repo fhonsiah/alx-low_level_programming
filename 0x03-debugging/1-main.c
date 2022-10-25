0ab6aa432fabb3aa886ea7bf37c1c0b40e1ff110
fhonsiah <fhonsiah@gmail.com>
2022-10-25 04:05:42 -0700

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,14 +9,14 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n"));
-        /*
+        
 	int = 0;
-
+/**
 	while (i < 10)
 	{
 		putchar(i);
 	}
-        */
+*/
 	printf("Infinte loop avoided! \\o/\n");
 
 	return (0);
