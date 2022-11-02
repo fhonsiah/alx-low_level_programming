dacad8716efadf6f72277ebbca46d656dd010199
fhonsiah <fhonsiah@gmail.com>
2022-11-02 06:50:30 -0700

    Learning Pointers,arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,20 @@
+/**
+ *Program: 0-reset_to_98.c
+ *A function that takes a pointer to an int as parameter and updates
+ *the value it points to 98.
+ * 
+ *Author: Faustina Honsiah
+ */
+#include "main.h"
+/**
+ *reset_to_98 - resets and updates an integer
+ *
+ *@n: The parameter to be updated
+ * 
+ *Return: void
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+## 0x05. C - Pointers, arrays and strings
+
+# File Description
+
+ 0. A function that takes a pointer to an int as parameter and updates the value it points to to 98
