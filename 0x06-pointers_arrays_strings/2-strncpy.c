59e2e280f82a2e22236149763571e276cb907b6c
fhonsiah <fhonsiah@gmail.com>
2022-11-03 06:16:14 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+/**
+ *Program: 2-strncpy.c
+ *A function that copies a string.
+ *Author: Faustina Honsiah.
+ */
+#include "main.h"
+#include <string.h>
+/**
+ *_strncpy - function that copies a string.
+ *@dest: the pointer parameter for destination string
+ *@src: the pointer parameter for source string
+ *@n: the number of characters to concatenate.
+ *Return: a pointer string.
+ */
+char *_strncpy(char *dest, char *src,int n)
+{
+	return (strncpy(dest,src,n));
+}
