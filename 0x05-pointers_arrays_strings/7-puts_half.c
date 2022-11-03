4b29d971d9d41bfd911d0deeec7a3e027947b11d
fhonsiah <fhonsiah@gmail.com>
2022-11-03 03:14:35 -0700

    Winning is only half of it. Having fun is the other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+/**
+ *Program: 7-puts_half.c
+ *A function that prints half of the string
+ *Author: Faustina Honsiah
+ */
+#include "main.h"
+#include <string.h>
+/**
+ *puts_half - function that prints half of the string.
+ *
+ *@str: the parameter to be printed to std output.
+ *
+ *Return: void
+ */
+void puts_half(char *str)
+{
+	int length = strlen(str);
+	int half_length;
+
+	half_length = (length % 2 == 0) ? length / 2 : (length + 1) / 2;
+
+	while(str[half_length] != '\0')
+	{
+		_putchar(str[half_length] != );
+		half_length++;
+	}
+
+	_putchar('\n');
+}
