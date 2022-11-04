58cb603fe58c1e9ab9cee0011b83b8ceaedb7221
fhonsiah <fhonsiah@gmail.com>
2022-11-03 23:21:41 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,44 @@
+/**
+ *Program: 6-cap_string.c
+ *A function that capitalizes all words of a string.
+ *Author: Faustina Honsiah
+ */
+#include "main.h"
+/***
+ *cap_string - capitalizes all words of a string.
+ *@str: the string parameter to be evaluated.
+ *Return: a string with all words capitalized.
+ */
+char *cap_string(char *str)
+{
+	int i = 1;
+	int j;
+	int check;
+	char a[] = {',',';', '.', '!', '?', '"', '(', ')','{', '}', '\n', '\t',' '
+	};
+
+	if (str[0] > 96 && str[0] < 123)
+		str[0] -= 32;
+	while (str[i] != '\0')
+	{
+		if (str[i] > 96 && str[i] < 123)
+		{
+			j = 0;
+			check = 0;
+			while (check == 0 && j < 13)
+			{
+				if (str[i - 1] == a[j])
+				{
+					check = 1;
+				}
+				j++;
+			}
+			if (check == 1)
+			{
+				str[i] -= 32;
+			}
+		}
+		i++;
+	}
+	return (str);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -9,3 +9,4 @@
  4. A function that reverses the content of an array of integers.
  5. A function that changes all lowercase letters of a string to uppercase.
  6. A function that capitalizes all words of a string.
+ 7. A function that encodes a string into 1337.
