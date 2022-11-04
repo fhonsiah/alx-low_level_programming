a8e58c3cbfb866b3b900645a1e8523b0c0c8a6fc
fhonsiah <fhonsiah@gmail.com>
2022-11-04 02:28:43 -0700

    It is the addition of strangeness to beauty that constitutes the romantic character in art

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,83 @@
+/**
+ *Program: 103-infinte_add.c
+ *A function that prints an integer.
+ *Author: Faustina Honsiah
+ */
+#include "main.h"
+/**
+ * string_reverse - A function for reversint the array string
+ *@n: A character pointer parameter.
+ *Return: void
+ */
+void string_reverse(char *n)
+{
+	int i = 0;
+	int k = 0;
+	char tmp;
+
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	for (k = 0; k < i; k++; i--)
+	{
+		tmp = *(n + k);
+		*(n + k) = *(n + i);
+		*(n + i) = tmp;
+	}
+}
+/**
+ *infinite_add - A function that adds two numbers.
+ *@n1: the first number.
+ *@n2: the second number.
+ *@r: the parameter for storing the result
+ *@size)R: the buffer size
+ *Return: a character pointer to the result
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int overflow = 0;
+	int i = 0;
+	int k = 0;
+	int digits = 0;
+	int value1 = 0, value2 = 0, tmp_total = 0;
+
+	while(*(n1 + i) != '\0')
+		i++;
+	while(*(n2 + k) != '\0')
+		k++;
+
+	i--;
+	k--;
+	if (k >= size_r || i >= size_r)
+		return (0);
+	while (k >= 0 || i >= 0 || overflow == 1)
+	{
+		if(i < 0)
+			value1 = 0;
+		else
+			value = *(n1 + i) - '0';
+		if(k < 0)
+			value2 = 0;
+		else
+			value2 = *(n2 + k) - '0';
+		tmp_total = value1 + value2 + overflow;
+		if (tmp_total >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if(digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (tmp_total % 10) + '0';
+		digits++;
+		k--;
+		i--;
+	}
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	string_reverse(r);
+	return (r);
+}
