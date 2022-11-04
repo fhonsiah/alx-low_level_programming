7d5792a504b077bf1283586664dad157ec4b6ac3
fhonsiah <fhonsiah@gmail.com>
2022-11-04 00:31:15 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,33 @@
+/**
+ *Program: 100-rot13.c
+ *A function that encodes a string using rot13.
+ *Author: Faustina Honsiah
+ */
+#include "main.h"
+/**
+ *rot13 - encodes a string using rot13
+ *@s: the string parameter to be encoded.
+ *Retrun: a string which is encoded.
+ */
+char *rot13(char *s)
+{
+	int x = 0;
+	int y = 0;
+	char stirng_rot13[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
+	char string_alpha[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+
+	while(s[x] != '\0')
+	{
+		do {
+			if (s[x] == string_alpha[y])
+			{
+				s[x] = string_rot13[y];
+				break;
+			}
+			y++;
+		} while (string_alpha[y] != '\0');
+		y = 0;
+		x++;
+	}
+	return (s);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -10,3 +10,6 @@
  5. A function that changes all lowercase letters of a string to uppercase.
  6. A function that capitalizes all words of a string.
  7. A function that encodes a string into 1337.
+ 8. A function that encodes a string using rot13.
+ 9. A function that prints an integer.
+ 10. Add one line to this code, so that the program prints a[2] = 98, followed by a new line.     
