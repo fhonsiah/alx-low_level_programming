ba72de618738f22c3578df8e58081e8b33631b2e
fhonsiah <fhonsiah@gmail.com>
2022-11-07 03:45:07 -0800

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,16 +1,20 @@
 /**
- *Program: 7-print_chessboard.c
- *A function that prints the chessboard
+ *Program: 5-strstr.c
+ *A function that locates a substring
  *Author: Faustina Honsiah
  */
 #include "main.h"
 /**
- *print_chessboard -  function that prints the chessboard
- *@a: the pointer for the two dimensianol array
+ *_strstr - function that locates a substring
+ *@haystack: the pointer string.
+ *@needle: the character occured.
+ *Return: a substring.
+ *
  */
 char *strstr(char *haystack, char *needle)
 {
 int a = 0, b = 0;
+
 while (haystack[a])
 {
 while (needle[b])
