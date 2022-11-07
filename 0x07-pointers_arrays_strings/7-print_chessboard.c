d6caebafefbdd98f5a0998e410f62e203a254331
fhonsiah <fhonsiah@gmail.com>
2022-11-07 03:38:46 -0800

    Chess is mental torture

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+/**
+ *Program: 7-print_chessboard.c
+ *A function that prints the chessboard
+ *Author: Faustina Honsiah
+ */
+#include "main.h"
+/**
+ *print_chessboard -  function that prints the chessboard
+ *@a: the pointer for the two dimensianol array
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for(i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)I
+			_putchar(a[i][j]);
+               _putchar('\n');
+	}
+}
