f8e505c185f30b24ab2e7c74e3a1eafd826ab3ff
fhonsiah <fhonsiah@gmail.com>
2022-11-21 00:54:25 -0800

    Once an idea has taken hold of the brain it's almost impossible to eradicate

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_pow_recursion -  A function that returns factorial of a number.
+ *@x: An integer to be powered
+ *@y: A power.
+ *Return: the powered number
+ *
+ */
+int _pow_recursion(int x, int y)
+{
+	if(y < 0)
+		return (-1);
+	if(y == 0)
+		return (1);
+	return (x * _pow_recursion(x,y - 1));
+}
