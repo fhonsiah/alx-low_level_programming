c30c2662a44f40ab18018ce3eace481d01ecc9b3
fhonsiah <fhonsiah@gmail.com>
2022-11-22 00:36:34 -0800

    How to use arguments passed to your program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+/**
+ *A program that prints its name followed by a new line
+ *Author: Faustina Honsiah <fhonsiah@gmail.com>
+ */
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - An entry point
+ *@argc: argument counter
+ *@argv: argument vector
+ *Return: always return success
+ */
+int main(int argc, char **argv)
+{
+	if (argc > 0)
+		printf("%\n", *argv);
+	return (0);
+}
