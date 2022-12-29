d0bf3a1ff6c45978f6c20e8e1c0fa910af145725
fhonsiah <fhonsiah@gmail.com>
2022-12-29 00:34:35 -0800

    Creating a dog structure 0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,27 @@
-#include <stdio.h>
-#include "dog.h"
+#ifndef HEADER
+#define HEADER
+
 /**
- *init_dog - initializes a variable of type struct dog
+ *struct dog - A dog struct
+ *@name: First member
+ *@age: Second member
+ *@owner: Third member
  *
- *@d: pointer to struct dog to initialize
- *@name: name to initialize
- *@age: age to initialize
- *@owner : owner to initialize
+ *Description: Creating a dog structure
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+struct dog
 {
-	if (d)
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
-}
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
