de3439f35d55e2492252d1abfa842688686ce898
fhonsiah <fhonsiah@gmail.com>
2022-12-30 00:48:42 -0800

    Learning function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ *print_name - Function to call a function
+ *@name: Parameter 1
+ *@f: parameter function
+ *Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f)
+	{
+		if (name)
+		{
+			(*f)(name);
+		}
+	}
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <stdlib.h>
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+
+
+#endif
